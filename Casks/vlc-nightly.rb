798249332d9ee3e5d2a186b34fd1b7be4ef59cb1
Mark Griffiths <mark@thebespokepixel.com>
2016-08-05 04:08:41 +0100

    Initial Commit

---
+++ Casks/vlc-nightly.rb
@@ -0,0 +1,25 @@
+cask 'vlc-nightly' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://nightlies.videolan.org/build/macosx-intel/last'
+  name 'VLC media player'
+  homepage 'https://www.videolan.org/vlc/'
+  license :oss
+
+  depends_on macos: '>= :lion'
+
+  app 'VLC.app', target: 'VLC (Nightly).app'
+
+   # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/vlcwrapper"
+  binary shimscript, target: 'nvlc'
+
+  preflight do
+    File.open(shimscript, 'w') do |f|
+      f.puts '#!/bin/bash'
+      f.puts "#{appdir}/VLC (Nightly).app/Contents/MacOS/VLC \"$@\""
+      FileUtils.chmod '+x', f
+    end
+  end
+end
---
+++ readme.md
@@ -0,0 +1,5 @@
+# The Bespoke Tap
+
+A Collection of modified or tuned Hombrew formulae, casks and commands I use in production.
+
+Will also contain my own apps and cli tools in due course.
